4e665f3e16db5174b3bf0c5c5c6d1f78d7b20cd2
刘进行 <jackingstarliu@gmail.com>
2019-05-28 15:55:53 +0800

    Updates

---
+++ precache-manifest.2747a408aa8e0a77690c1735d5a85995.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec791b1ee547ffdae24c7cabfa571091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfc6f3c556e3021e8b5c",
+    "url": "/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "8b7c207c3da0720eb9cb",
+    "url": "/static/js/2.9c558866.chunk.js"
+  },
+  {
+    "revision": "bfc6f3c556e3021e8b5c",
+    "url": "/static/js/main.5eddbe29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
